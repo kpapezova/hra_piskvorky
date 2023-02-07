9b956c8d9859a891b40816791862cfde17c4718f
kpapezova <papezka.kristyna@gmail.com>
2023-02-07 22:14:01 +0100

    Fce tah presunuta do util.py

--- piskvorky.py
+++ piskvorky.py
@@ -7,41 +7,15 @@
 # "-" – Ani jedna ze situací výše (t.j. hra ještě neskončila)
 
 
-
-
 def vyhodnot (pole):
     """Funkce dostane řetězec s herním polem 1-D piškvorek a vrátí jednoznakový řetězec, podle stavu hry"""
-    if "xxx" in pole:
+    if "xxx" in pole:   # vitez "x"
         return "x"
-    elif "ooo" in pole:
+    elif "ooo" in pole: # vitez "o"
         return "o"
-    elif "-" not in pole:
+    elif "-" not in pole:   # remíza
         return "!"
-    else:
+    else:   # ani jedna ze situací výše
         return "-"
 
-print(vyhodnot("xoxoxxoxoxoxoxoxo"))
-
-
-
-
-#Napiš funkci tah, která dostane řetězec s herním polem, číslo políčka (0-19), a symbol (x nebo o), a vrátí herní pole (t.j. řetězec) s daným symbolem umístěným na danou pozici.
-
-def tah (pole, cislo_policka, symbol):
-    """Vrátí herní pole s daným symbolem umístěným na danou pozici"""
-    pozice = 0
-    # for cyklus prochází postupně jednotlivá políčka pole od pozice nula. Pokud se při procházení pozice rovná cislu policka, pak vyskoci z funkce a vrátí nové pole se zapsaným symbolem na dané pozici" 
-    for i in pole:
-        print(pozice)
-        if pozice == cislo_policka:
-            nove_pole = pole[:cislo_policka] + symbol + pole[cislo_policka+1 :]
-            return(nove_pole)
-        else:
-            pozice = pozice + 1 
-            print(pozice)
-
-
-
-print(tah("---xo--", 0, "x"))
-print(tah("---xo--", 3, "o"))       # ve fci není ošetřeno, jestli je políčko volné. Takže tento tah přepíše "x" na "o". Bude řešeno
-
+print(vyhodnot("xoxxoooxoxo-oxoxo"))
